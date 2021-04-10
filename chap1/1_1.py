a6f5ab386755b8d46913f4bb0773e552c939f94c
kitakou0313 <kitakou0313@gmail.com>
2021-04-10 07:03:30 +0000

    Add other answer

--- chap1/1_1.py
+++ chap1/1_1.py
@@ -10,6 +10,15 @@ def isSpecific(inputtedStr):
         appearedChars.add(char)
 
     return True
+
+def isSpecificWithoutOtherDataStructure(inputtedStr):
+    sortedStr = sorted(inputtedStr)
+
+    for i in range(len(sortedStr) - 1):
+        if sortedStr[i] == sortedStr[i + 1]:
+            return False
+    
+    return True
         
 
 
@@ -18,6 +27,11 @@ class Test(unittest.TestCase):
         self.assertEqual(isSpecific("abcdefg"), True)
         self.assertEqual(isSpecific("aaaaaa"), False)
         self.assertEqual(isSpecific(""), True)
+
+    def test_isSpecificOtherDataStructure(self):
+        self.assertEqual(isSpecificWithoutOtherDataStructure("abcdefg"), True)
+        self.assertEqual(isSpecificWithoutOtherDataStructure("aaaaaa"), False)
+        self.assertEqual(isSpecificWithoutOtherDataStructure(""), True)
 
 if __name__ == "__main__":
     unittest.main()
