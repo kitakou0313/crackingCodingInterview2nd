55cbd8d46d63442c347e46e0a16f1818f2ee4073
kitakou0313 <kitakou0313@gmail.com>
2021-04-26 13:28:54 +0000

    Add stack to lib

---
+++ libs/stack.py
@@ -0,0 +1,37 @@
+import copy
+
+class IntStack(object):
+    """
+    int用Stack
+    """
+    def __init__(self):
+        """
+        コンストラクタ
+        """
+        self.data:[int] =[]
+
+    def pop(self) -> int:
+        """
+        出す
+        """
+        return self.data.pop()
+    
+    def add(self, value:int) -> None:
+        """
+        入れる
+        """
+        self.data.append(value)
+
+    def isEmpty(self) -> bool:
+        """
+        要素なしか返す
+        """
+        return len(self.data) == 0
+
+    def len(self) -> int:
+        """
+        長さ返却
+        """
+        return len(self.data)
+        
+        
