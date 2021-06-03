e96200ee37f6b1f3f8c86903798f73b29e2de0ba
kitakou0313 <kitakou0313@gmail.com>
2021-06-03 15:33:29 +0000

    Add ans of 3_6

---
+++ chap3/3_6.py
@@ -0,0 +1,121 @@
+from typing import List
+import unittest
+import time
+
+class Animal(object):
+    """
+    動物クラス
+    """
+    def __init__(self, name:str):
+        """
+        コンストラクタ
+        """
+        self.__name:str = name
+        self.__time:float = time.time()
+
+    def getTime(self) -> float:
+        return self.__time
+
+class Dog(Animal):
+    """
+    犬クラス
+    """
+    def __init__(self, name: str):
+        super().__init__(name)
+    
+    
+
+class Cat(Animal):
+    """
+    猫クラス
+    """
+    def __init__(self, name: str):
+        super().__init__(name)
+
+class AnimalQueue(object):
+    """
+    DogClass専用のQueue
+    """
+    def __init__(self):
+        """
+        コンストラクタ
+        """
+        self.__animals:List[Animal] = []
+
+    def pop(self) -> Animal:
+        """
+        Pop
+        """
+        return self.__animals.pop(0)
+    
+    def push(self, animal:Animal) -> None:
+        """
+        Push
+        """
+        self.__animals.append(animal)
+
+    def isEmpty(self):
+        """
+        空か返す
+        """
+        return len(self.__animals) == 0
+    
+    def top(self) -> Animal:
+        return self.__animals[0]
+
+
+class AnimalShelter(object):
+    """
+    動物病院クラス
+    """
+    def __init__(self) -> None:
+        self.__dogQueue:AnimalQueue = AnimalQueue()
+        self.__catQueue:AnimalQueue = AnimalQueue()
+
+    
+    def euqueue(self, animal:AnimalQueue) -> None:
+        if type(animal) == Dog:
+            self.__dogQueue.push(animal)
+
+        if type(animal) == Cat:
+            self.__catQueue.push(animal)
+    
+    def dequeueCat(self) -> Animal:
+        """
+        Catを出す
+        """
+        return self.__catQueue.pop()
+    
+    def dequeueDog(self) -> Animal:
+        """
+        Dogを出す
+        """
+        return self.__dogQueue.pop()
+
+    def dequeueAny(self) -> Animal:
+        if self.__dogQueue.isEmpty() and self.__catQueue.isEmpty():
+            return None
+        
+        elif not(self.__dogQueue.isEmpty()) and self.__catQueue.isEmpty():
+            return self.__dogQueue.pop()
+
+        elif self.__dogQueue.isEmpty() and not(self.__catQueue.isEmpty()):
+            return self.__catQueue.pop()
+
+        else:
+            if self.__dogQueue.top().getTime() < self.__catQueue.top().getTime():
+                return self.__dogQueue.pop()
+            else:
+                return self.__catQueue.pop()
+            
+            
+            
+
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        print('')
+
+if __name__ == "__main__":
+    unittest.main()
