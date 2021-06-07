ed522d17af44670d8e69080b75b07f2ce8f7baff
kitakou0313 <kitakou0313@gmail.com>
2021-06-07 14:22:05 +0000

    Add templete of 4_5

---
+++ chap4/4_5.py
@@ -0,0 +1,18 @@
+import unittest
+from libs.tree import TreeNode
+
+def isBST(root:TreeNode) -> bool:
+    return True
+
+class Test(unittest.TestCase):
+    """
+    docstring
+    """
+    def Test1(self):
+        """
+        単体テスト
+        """
+
+
+if __name__ == "__main__":
+    unittest.main()
