31285c42eab4c40f2e7922760ee23a8e857f6a1b
kitakou0313 <kitakou0313@gmail.com>
2021-06-11 15:00:22 +0000

    Add temp ans

--- chap4/4_7.py
+++ chap4/4_7.py
@@ -1,8 +1,44 @@
-from typing import List, Dict, Tuple
+from typing import List, Dict, Set, Tuple
+from libs.queue import StrQueue
 import unittest
 
-def calExecutionOrder(projects:List[str], graph:Dict[Tuple[str]]) -> List[str]:
-    return 
+# 依存先->依存元のグラフを作り、幅探索して全ノード到達できたか探索
+def calExecutionOrder(projects:List[str], dependencies:List[Tuple[str]]) -> List[str]:
+    notDependingPros:Set[str] = set()
+    graph:Dict[str, List[str]] = {}
+
+    for pro in projects:
+        graph[pro] = []
+    
+    for depending, depended in dependencies:
+        if not(depended in graph):
+            graph[depended] = []
+        graph[depended].append(depending)
+
+    for pro in projects:
+        if len(graph[pro]) == 0:
+            notDependingPros.add(pro)
+
+    projectOrder:List[str] = []
+    isDone:Set[str] = set()
+
+    for startPro in notDependingPros:
+        projectOrder.append(startPro)
+        isDone.add(startPro)
+
+        q = StrQueue()
+        q.push(startPro)
+
+        while not(q.isEmpty()):
+            nowPro = q.pop()
+            for nxtWork in graph[nowPro]:
+                if nxtWork in isDone:
+                    continue
+                
+                q.push(nxtWork)
+                projectOrder.append(nxtWork)
+
+    return projectOrder
 
 class Test(unittest.TestCase):
     def test1(self):
