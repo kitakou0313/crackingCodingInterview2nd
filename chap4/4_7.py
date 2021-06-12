63c2fa4fe8f56dd8994cc028905bf11d5012630f
kitakou0313 <kitakou0313@gmail.com>
2021-06-12 07:33:55 +0000

    Add node class

--- chap4/4_7.py
+++ chap4/4_7.py
@@ -1,6 +1,30 @@
 from typing import List, Dict, Set, Tuple
-from libs.queue import StrQueue
 import unittest
+import heapq
+
+class nodeWithInNum(object):
+    """
+    入次数付有向グラフノード
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.__inNum:int = 0
+        self.__nextNodes:List[nodeWithInNum] = []
+
+    def getInNum(self) -> int:
+        return self.__inNum
+    
+    def getNextNodes(self) -> List['nodeWithInNum']:
+        return self.__nextNodes
+
+    def addNextNode(self, node:'nodeWithInNum'):
+        """
+        次ノード追加
+        """
+        self.__nextNodes.append(node)
+
+def constructGrapth(dependencies:List[Tuple[str]]):
+    return 
 
 # 依存先->依存元のグラフを作り、トポロジカルソートして返す
 def calExecutionOrder(projects:List[str], dependencies:List[Tuple[str]]) -> List[str]:
