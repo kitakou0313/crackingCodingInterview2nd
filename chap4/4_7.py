78321a6ac8681a5ff27dbebe0ff876339209ac34
kitakou0313 <kitakou0313@gmail.com>
2021-06-12 07:13:23 +0000

    Update kousatu

--- chap4/4_7.py
+++ chap4/4_7.py
@@ -2,42 +2,9 @@ from typing import List, Dict, Set, Tuple
 from libs.queue import StrQueue
 import unittest
 
-# 依存先->依存元のグラフを作り、幅探索して全ノード到達できたか探索
+# 依存先->依存元のグラフを作り、トポロジカルソートして返す
 def calExecutionOrder(projects:List[str], dependencies:List[Tuple[str]]) -> List[str]:
-    notDependingPros:Set[str] = set()
-    graph:Dict[str, List[str]] = {}
-
-    for pro in projects:
-        graph[pro] = []
-    
-    for depending, depended in dependencies:
-        if not(depended in graph):
-            graph[depended] = []
-        graph[depended].append(depending)
-
-    for pro in projects:
-        if len(graph[pro]) == 0:
-            notDependingPros.add(pro)
-
     projectOrder:List[str] = []
-    isDone:Set[str] = set()
-
-    for startPro in notDependingPros:
-        projectOrder.append(startPro)
-        isDone.add(startPro)
-
-        q = StrQueue()
-        q.push(startPro)
-
-        while not(q.isEmpty()):
-            nowPro = q.pop()
-            for nxtWork in graph[nowPro]:
-                if nxtWork in isDone:
-                    continue
-                
-                q.push(nxtWork)
-                projectOrder.append(nxtWork)
-
     return projectOrder
 
 class Test(unittest.TestCase):
@@ -56,7 +23,7 @@ class Test(unittest.TestCase):
 
         actual = calExecutionOrder(projects, dependencies)
 
-        for project, depended in dependencies:
+        for depended, project in dependencies:
             assert actual.index(depended) < actual.index(project)
 
 if __name__ == "__main__":
