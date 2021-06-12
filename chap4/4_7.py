7410d3b4edb0ac3dc594683ee874ac9b1ff64ffb
kitakou0313 <kitakou0313@gmail.com>
2021-06-12 08:35:18 +0000

    Add answer of 4_7

--- chap4/4_7.py
+++ chap4/4_7.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Set, Tuple
 import unittest
-import heapq
+from libs.queue import StrQueue
 
 def constructGraph(projects:List[str],dependencies:List[Tuple[str]]) -> Dict[str, List[str]]:
     graph:Dict[str, List[str]] = {}
@@ -26,27 +26,35 @@ def countInMum(projects:List[str], dependencies:List[Tuple[str]]) -> Dict[str, i
 
     return inNumDict
 
-class PriQueueEntry(object):
-    """
-    優先度付キュー用のエントリクラス
-    """
-    def __init__(self, data:str, inNum:int) -> None:
-        super().__init__()
-        self.data = data
-        self.inNum = inNum
-
-    def __lt__(self, other:'PriQueueEntry') -> bool:
-        return self.inNum < other.inNum
-
+def findProsToDo(q:StrQueue, inNumDict:Dict[str, int], didPros:Set[str]) -> None:
+    for pro, inNum in inNumDict.items():
+        if pro in didPros:
+            continue
+        if inNum == 0:
+            q.push(pro)
+            didPros.add(pro)
 
 # 依存先->依存元のグラフを作り、トポロジカルソートして返す
 def calExecutionOrder(projects:List[str], dependencies:List[Tuple[str]]) -> List[str]:
     graph = constructGraph(projects, dependencies)
     inNumDict = countInMum(projects, dependencies)
 
-    priQ:List[]
+    proToDo = StrQueue()
+
+    didPros:Set[str] = set()
+    findProsToDo(proToDo, inNumDict, didPros)
 
     projectOrder:List[str] = []
+
+    while not(proToDo.isEmpty()):
+        proToDoNow = proToDo.pop()
+        projectOrder.append(proToDoNow)
+
+        for nxtPro in graph[proToDoNow]:
+            inNumDict[nxtPro] -= 1
+
+        findProsToDo(proToDo, inNumDict, didPros)
+
     return projectOrder
 
 class Test(unittest.TestCase):
