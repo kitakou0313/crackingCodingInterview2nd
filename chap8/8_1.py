3c9a6642ca99f0af285b82c734c404c51a2b995a
kitakou0313 <kitakou0313@gmail.com>
2021-06-14 13:57:10 +0000

    Add ans of 8_1

--- chap8/8_1.py
+++ chap8/8_1.py
@@ -1,10 +1,26 @@
+from typing import List
 import unittest
 
+def calNumOfWaysToNstep(N:int) -> int:
 
+    # dp[n]...nまでの行き方
+    dp:List[int] = [0 for _ in range(N+1)]
+    dp[0] = 1
+
+    for n in range(0, N + 1):
+        if n + 1 <= N:
+            dp[n+1] += dp[n]
+        if n + 2 <= N:
+            dp[n+2] += dp[n]
+        if n + 3 <= N:
+            dp[n+3] += dp[n]        
+
+    return dp[N]
 
 class Test(unittest.TestCase):
     def test1(self):
-        pass
+        self.assertEqual(calNumOfWaysToNstep(3), 4)
+        self.assertEqual(calNumOfWaysToNstep(7), 44)
 
 if __name__ == "__main__":
     unittest.main()
