8fed7f128c982cfdda6c23c2924bc9ef349de500
kitakou0313 <kitakou0313@gmail.com>
2021-06-20 03:09:00 +0000

    Add ans of 8_7

--- chap8/8_7.py
+++ chap8/8_7.py
@@ -1,10 +1,21 @@
-from typing import Set
+from typing import Set, List
 import unittest
 import itertools
-from unittest.case import TestCase
 
-def generateAllPermutationOfString(string:str) -> Set[str]:
-    pass
+def generateAllPermutationOfString(inputStr:str) -> Set[str]:
+    if len(inputStr) == 1:
+        return set([inputStr])
+
+    res:Set[str] = set()
+
+    chars:List[str] = list(inputStr)
+
+    for charInd in range(0, len(chars)):
+        permsOfRestString = generateAllPermutationOfString("".join(chars[:charInd] + chars[charInd+1:]))
+        for perm in permsOfRestString:
+            res.add(chars[charInd] + perm)
+
+    return res
 
 class Test(unittest.TestCase):
     def test1(self):
