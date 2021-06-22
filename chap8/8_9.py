064226002a24aa8c9d9c722177e227309397c8a2
kitakou0313 <kitakou0313@gmail.com>
2021-06-22 13:52:22 +0000

    Add ans of 8_9

--- chap8/8_9.py
+++ chap8/8_9.py
@@ -2,7 +2,21 @@ from typing import Set
 import unittest
 
 def generateAllParensPattern(parensNum:int) -> Set[str]:
-    pass
+    if parensNum == 1:
+        return set(["()"])
+
+    #括弧の中、外でそれぞれ再帰的に追加していく
+    parenPatterns = generateAllParensPattern(parensNum - 1)
+
+    res:Set[str] = set()
+
+    for paren in parenPatterns:
+        res.add("()" + paren)
+        res.add(paren + "()")
+        res.add("(" + paren +")")
+
+    return res
+
 
 class Test(unittest.TestCase):
     def test1(self):
