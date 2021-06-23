fafa6faed1fdf1566d472e7875312067852514c9
kitakou0313 <kitakou0313@gmail.com>
2021-06-23 15:17:59 +0000

    Add ans of 8_10

--- chap8/8_10.py
+++ chap8/8_10.py
@@ -1,7 +1,36 @@
+from typing import Set, Tuple
 import unittest
+import copy
+from libs.queue import TupleQueue
 
 def fillMapWithColor(map, pos, color):
-    pass
+    res = copy.deepcopy(map)
+
+    q = TupleQueue()
+    isVisited:Set[Tuple[int]] = set()
+
+    dyVec = [0,1,0,-1]
+    dxVec = [1,0,-1,0]
+
+    oriColor = map[pos[0]][pos[1]]
+
+    q.push(pos)
+    isVisited.add(pos)
+
+    while not(q.isEmpty()):
+        nowY, nowX = q.pop()
+        res[nowY][nowX] = color
+
+        for dVecInd in range(len(dxVec)):
+            nxtY = nowY + dyVec[dVecInd]
+            nxtX = nowX + dxVec[dVecInd]
+
+            if 0 <= nxtY <= len(res)-1 and 0 <= nxtX <= len(res[0])-1:
+                if res[nxtY][nxtX] == oriColor:
+                    q.push((nxtY, nxtX)) 
+                    isVisited.add((nxtY, nxtX))
+
+    return res
 
 class Test(unittest.TestCase):
     def test1(self):
