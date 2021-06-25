1077096c3c8724e3eba537694570a2817f1ef032
kitakou0313 <kitakou0313@gmail.com>
2021-06-25 14:24:09 +0000

    Add testcase for 8_11

--- chap8/8_11.py
+++ chap8/8_11.py
@@ -1,8 +1,21 @@
+from typing import List
 import unittest
+
+def calculateCoins(value:int, coins:List[int]):
+    pass
 
 class Test(unittest.TestCase):
     def test1(self):
-        pass
+        testCases = [
+            10, 20, 50
+        ]
+
+        coins = [
+            1,5,10,25
+        ]
+
+        for n in testCases:
+            print(calculateCoins(n, coins))
 
 if __name__ == "__main__":
     unittest.main()
