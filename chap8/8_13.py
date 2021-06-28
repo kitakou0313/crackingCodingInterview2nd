f54d0d9a91958e23ee88b0c627488855bff07565
kitakou0313 <kitakou0313@gmail.com>
2021-06-28 13:56:59 +0000

    Add test cases

--- chap8/8_13.py
+++ chap8/8_13.py
@@ -19,7 +19,8 @@ class Test(unittest.TestCase):
     def test1(self):
         testCases = [
             ([Box(4,3,2),Box(5,4,1)], 5),
-            ([Box(3,2,1), Box(6,5,4)],9)
+            ([Box(3,2,1), Box(6,5,4)],9),
+            ([Box(100, 100, 100), Box(25, 25, 25), Box(20, 5, 30), Box(17, 4, 28)], 137)
         ]
         
         for boxes, expected in testCases:
