ac1b4f587fd1a915978825d2b247986c56c88c79
kitakou0313 <kitakou0313@gmail.com>
2021-07-03 06:19:37 +0000

    Add ans of 18 17

--- chap16/16_17.py
+++ chap16/16_17.py
@@ -3,11 +3,20 @@ import unittest
 
 #2重ループで全区間探索O(n^2)
 def searchSumMaxInterval(array:List[int]) -> int:
-    pass
+    maxSum = - float("inf")
+
+    for intervalStartInd in range(0,len(array)):
+        sumInInterval = 0
+        for intervalEndInd in range(intervalStartInd, len(array)):
+            sumInInterval += array[intervalEndInd]
+            maxSum = max(maxSum, sumInInterval)
+
+    return maxSum
+
 class Test(unittest.TestCase):
     def test1(self):
         testCases = [
-            ([2,-8,3,-2,4,10], 5)
+            ([2,-8,3,-2,4,-10], 5)
         ]
 
         for array, expected in testCases:
