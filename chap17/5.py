79c1cdef25223a5f7e176b016e0eed8c0dba8b2e
kitakou0313 <kitakou0313@gmail.com>
2021-09-25 00:50:34 +0900

    Add templete code for 5

---
+++ chap17/5.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Test(unittest.TestCase):
+    """
+    docstring
+    """
+    def test_1(self):
+        self.assertEqual(1,1)
+
+if __name__ == "__main__":
+    unittest.main()
