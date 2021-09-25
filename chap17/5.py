cd5dcf38626e671a5535ed48fd1c9863e6e6cd84
kitakou0313 <kitakou0313@gmail.com>
2021-09-25 05:30:44 +0000

    Define test caes

--- chap17/5.py
+++ chap17/5.py
@@ -1,13 +1,20 @@
 import unittest
+from unittest.case import TestCase
 
 # 含まれる文字と数字の数が等しくなるような最長部分配列の探索
+# 文字は2種類だけでよい
 
 class Test(unittest.TestCase):
     """
     docstring
     """
     def test_1(self):
-        self.assertEqual(1,1)
+        testCase = [
+            (["A", "B", "A", "A", "B", "B"],6),
+            (["A", "B", "A", "B", "B", "B"], 4),
+            (["B", "B", "B", "A", "B", "A"], 4)
+        ]
+        
 
 if __name__ == "__main__":
     unittest.main()
