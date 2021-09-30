8436996b7a268404c4ed2cd43a2c2b1eb23a13a1
kitakou0313 <kitakou0313@gmail.com>
2021-09-30 03:01:37 +0000

    Add ans

--- chap17/7.py
+++ chap17/7.py
@@ -1,13 +1,14 @@
 from typing import Dict, List, Set, Tuple
 import unittest
 from unittest.case import TestCase
+from libs.queue import StrQueue
 
 def solver(nameAndFreq:List[tuple], synonyms:List[tuple]) -> Dict[str, int]:
     """
     シノニムの関係のグラフ構築
     各名前について
-    １．シノニムをすべて上のグラフから取得
-    ２．各シノニムについてすでにシノニムが存在していれば可算、辞書に追加
+    １．結果用の辞書型の各名前のシノニムに含まれるか検索
+    ２．すでにシノニムが存在していれば可算、なければ辞書に追加
     """
     def createSynonymGraph(synonyms:List[tuple]) -> Dict[str, Set[str]]:
         """
@@ -29,9 +30,49 @@ def solver(nameAndFreq:List[tuple], synonyms:List[tuple]) -> Dict[str, int]:
 
         return synonymGraph
 
+    def findAllSynonym(name:str, synonymGraph:Dict[str, Set[str]]) -> Set[str]:
+        """
+        nameのシノニムを幅探してすべて返す
+        """
+        synonymSet:Set[str] = set()
+
+        queue = StrQueue()
+        isVisited:Set[str] = set()
+
+        queue.push(name)
+        isVisited.add(name)
+
+        while not(queue.isEmpty()):
+            nowName = queue.pop()
+            for nxtName in synonymGraph[nowName]:
+                if nxtName not in isVisited:
+                    synonymSet.add(nxtName)
+                    queue.push(nxtName)
+                    isVisited.add(nxtName)
+
+        return synonymSet
+
     synonymGraph = createSynonymGraph(synonyms)
 
     ansMap:Dict[str, int] = {}
+    keyAndSynonymsMap:Dict[str, Set[str]] = {}
+
+    for name, freq in nameAndFreq:
+        isSynonym = False
+        synonymName = ""
+        
+        for addedName in keyAndSynonymsMap:
+            if name in keyAndSynonymsMap[addedName]:
+                isSynonym = True
+                synonymName = addedName
+        
+        if isSynonym:
+            ansMap[synonymName] += freq
+        else:
+            ansMap[name] = freq
+            keyAndSynonymsMap[name] = findAllSynonym(name, synonymGraph)
+        
+
     return ansMap
     pass
 
