0fbd9ec05a830a20c68746864e0ddfff5513750e
kitakou0313 <kitakou0313@gmail.com>
2021-10-01 04:48:01 +0000

    Add testcases

--- chap17/6.py
+++ chap17/6.py
@@ -1,6 +1,12 @@
 import unittest
 from unittest.case import TestCase
 
+def countNumberOf2(N:int) -> int:
+    """
+    Nまでの自然数で文字列表記したときの2の個数
+    """
+    return 2
+
 class Test(TestCase):
     """
     docstring
@@ -9,7 +15,12 @@ class Test(TestCase):
         """
         docstring
         """
-        pass
+        testCases = [
+            (25, 9)
+        ]
+
+        for input, expected in testCases:
+            self.assertEqual(countNumberOf2(input), expected)
 
 if __name__ == "__main__":
     unittest.main()
