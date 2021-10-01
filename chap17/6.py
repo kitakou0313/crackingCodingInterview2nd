cbe352aa07f24cae8d2288096061745e084b2a41
kitakou0313 <kitakou0313@gmail.com>
2021-10-01 04:41:36 +0000

    Add templete for 6

---
+++ chap17/6.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.case import TestCase
+
+class Test(TestCase):
+    """
+    docstring
+    """
+    def test(self):
+        """
+        docstring
+        """
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
