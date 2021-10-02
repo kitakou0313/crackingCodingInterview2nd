670dc5ea5f215563a030d83c97269de40a984e12
kitakou0313 <kitakou0313@gmail.com>
2021-10-02 14:08:17 +0000

    Add testCases

--- chap17/8.py
+++ chap17/8.py
@@ -5,11 +5,16 @@ class Test(TestCase):
     """
     docstring
     """
-    def test(self, parameter_list):
+    def test(self):
         """
         docstring
         """
-        pass
+        testCases = [
+            ([(65,100),(70,150),(56,90),(75,190),(60,95),(68,110)],6)
+        ]
+
+        for inputted, expected in testCases:
+            print(inputted, expected) 
 
 if __name__ == "__main__":
     unittest.main()
