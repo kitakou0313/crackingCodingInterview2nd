fd6c81de3876a614fc11179b108fd7616281ae5a
kitakou0313 <kitakou0313@gmail.com>
2021-10-02 14:12:16 +0000

    Add test case

--- chap17/8.py
+++ chap17/8.py
@@ -1,5 +1,12 @@
+from typing import Tuple, List
 from unittest import TestCase
 import unittest
+
+def calMaxTowerHeight(peopleList:List[Tuple[int]]) -> int:
+    """
+    docstring
+    """
+    pass
 
 class Test(TestCase):
     """
@@ -13,8 +20,8 @@ class Test(TestCase):
             ([(65,100),(70,150),(56,90),(75,190),(60,95),(68,110)],6)
         ]
 
-        for inputted, expected in testCases:
-            print(inputted, expected) 
+        for inputtedCase, expected in testCases:
+            self.assertEqual(calMaxTowerHeight(inputtedCase), expected)
 
 if __name__ == "__main__":
     unittest.main()
