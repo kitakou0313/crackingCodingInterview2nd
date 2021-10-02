f385cadbede8bffffe17228c1c6d18e72a582c3f
kitakou0313 <kitakou0313@gmail.com>
2021-10-02 13:38:07 +0000

    Add templete

---
+++ chap17/8.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+import unittest
+
+class Test(TestCase):
+    """
+    docstring
+    """
+    def test(self, parameter_list):
+        """
+        docstring
+        """
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
