db0a07bae693d5b58f0445b0eb1df282bde6a3cc
kitakou0313 <kitakou0313@gmail.com>
2021-10-03 14:16:53 +0000

    Add dp code

--- chap17/8.py
+++ chap17/8.py
@@ -1,13 +1,32 @@
-from typing import Tuple, List
+from typing import Dict, Tuple, List
 from unittest import TestCase
 import unittest
+
+def calMaxHeightWithBottom(peopleList:List[Tuple[int]], bottom:Tuple[int] ,memo:Dict[Tuple[int], int]) -> int:
+    """
+    docstring
+    """
+    return 0
 
 def calMaxTowerHeight(peopleList:List[Tuple[int]]) -> int:
     """
     全順序、全選び方について探索 O(2^n * nCn)
     memo化で高速化出来そう 各人が底の時の最大の高さをmemoにする
     """
-    pass
+    maxHeight = -float("inf")
+    memo:Dict[Tuple[int], int] = {}
+
+    for i in range(0, len(maxHeight)):
+        lastPeoples = maxHeight[:i] + maxHeight[i+1:]
+        maxHeight = max(maxHeight, calMaxHeightWithBottom(lastPeoples, peopleList[i], memo))
+
+    return maxHeight
+
+def isStandOnA(A:Tuple[int], B:Tuple[int]) -> bool:
+    """
+    AがBの上に立てるか判定
+    """
+    return A[0] < B[0] and A[1] < B[1]
 
 class Test(TestCase):
     """
