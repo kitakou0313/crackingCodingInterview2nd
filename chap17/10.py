8864a1be04cee85e365803f5b8f352cb6c5bd286
kitakou0313 <kitakou0313@gmail.com>
2021-10-05 14:26:53 +0000

    Add testcase templete

---
+++ chap17/10.py
@@ -0,0 +1,14 @@
+import unittest
+
+class Test(unittest.TestCase):
+    """
+    docstring
+    """
+    def test(self):
+        """
+        docstring
+        """
+        self.assertEqual(1,2)
+
+if __name__ == "__main__":
+    unittest.main()
