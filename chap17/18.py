c3191543f7dd2e7c16bc9ed073a01af285cfe289
kitakou0313 <kitakou0313@gmail.com>
2021-10-08 04:34:16 +0000

    Install formatter

--- chap17/18.py
+++ chap17/18.py
@@ -1,7 +1,8 @@
 from typing import List, Set, Tuple
 import unittest
 
-def findShortestSubArrayIncludingElements(elements:Set[int], array:List[int]) -> Tuple[int]:
+
+def findShortestSubArrayIncludingElements(elements: Set[int], array: List[int]) -> Tuple[int]:
     """
     arrayの中からelements内の要素すべてを持つ最短の部分配列を探索、インデックスを返す
     brute force...O(n^2) 各インデックスに対して先頭から末尾まで探索、elementsをすべて含めば記録
@@ -29,13 +30,13 @@ def findShortestSubArrayIncludingElements(elements:Set[int], array:List[int]) ->
     shortestSubArrayEndInd = len(array)
 
     right = 0
-    elementsIncludedInSubArray:Set[int] = set()
+    elementsIncludedInSubArray: Set[int] = set()
 
     for left in range(len(array)):
         while (right < len(array) and not(elementsIncludedInSubArray.issuperset(elements))):
             elementsIncludedInSubArray.add(array[right])
             right += 1
-        
+
         if elementsIncludedInSubArray.issuperset(elements):
             if (shortestSubArrayEndInd - shortestSubArrayStartInd + 1) > right - left:
                 shortestSubArrayStartInd = left
@@ -45,22 +46,28 @@ def findShortestSubArrayIncludingElements(elements:Set[int], array:List[int]) ->
             right += 1
         else:
             elementsIncludedInSubArray.discard(array[left])
-        
+
     return (shortestSubArrayStartInd, shortestSubArrayEndInd)
+
+
 class Test(unittest.TestCase):
     """
     docstring
     """
+
     def test(self):
         """
         docstring
         """
         testCase = [
-            (set([1,5,9]), [7,5,9,0,2,1,3,5,7,9,1,1,5,8,8,9,7], (7,10))
+            (set([1, 5, 9]), [7, 5, 9, 0, 2, 1, 3,
+             5, 7, 9, 1, 1, 5, 8, 8, 9, 7], (7, 10))
         ]
 
         for elements, array, expected in testCase:
-            self.assertEqual(findShortestSubArrayIncludingElements(elements, array),expected)
+            self.assertEqual(findShortestSubArrayIncludingElements(
+                elements, array), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
